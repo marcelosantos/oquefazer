1c34994426a339d302f486330d9f3d85fcfe4d30
Marcelo Santos <marcelosantosadm@gmail.com>
2016-12-28 12:59:42 +0000

    adicionando CSRF para seguranca deploy django no heroku

--- static/js/oquefazer.config.js
+++ static/js/oquefazer.config.js
@@ -35,7 +35,7 @@ run.$inject = ['$http'];
 * @name run
 * @desc Update xsrf $http headers to align with Django's defaults
 */
-function run($http, $httpProvider) {
+function run($http) {
     $http.defaults.xsrfHeaderName = 'X-CSRFToken';
     $http.defaults.xsrfCookieName = 'csrftoken';
 };
